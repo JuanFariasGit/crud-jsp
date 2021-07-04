aa9ef85b6edb51ad81c47c7cd0847deca023c448
Juan Farias <juanfarias580@gmail.com>
2021-07-04 17:14:47 -0300

    changes in in AutenticacaoFilter

--- src/main/java/br/com/dominio/filter/AutenticacaoFilter.java
+++ src/main/java/br/com/dominio/filter/AutenticacaoFilter.java
@@ -19,11 +19,11 @@ public class AutenticacaoFilter implements Filter {
 
         HttpServletRequest httpServletRequest = (HttpServletRequest) req;
         String uri = httpServletRequest.getRequestURI();
-        String uriAdd = uri + "?action=" + req.getParameter("action");
+        String action = req.getParameter("action");
         HttpSession session = httpServletRequest.getSession();
         
         if (session.getAttribute("login-crud-jsp") != null || uri.endsWith("login.jsp")
-                || uri.endsWith("cadastro.jsp") || uriAdd.endsWith("usuarios?action=add")) {
+                || uri.endsWith("cadastro.jsp") || action.endsWith("add")) {
             chain.doFilter(req, resp);
         } else {
             ((HttpServletResponse) resp).sendRedirect("/login.jsp");
