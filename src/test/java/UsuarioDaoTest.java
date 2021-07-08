cb92af33881101974eb05be75862ffb97e6b3b67
Juan Farias <juanfarias580@gmail.com>
2021-07-07 22:38:00 -0300

    changes in UsuarioDaoTest and persistence

--- src/test/java/UsuarioDaoTest.java
+++ src/test/java/UsuarioDaoTest.java
@@ -1,70 +1,62 @@
 
-import br.com.dominio.dao.UsuarioDao;
-import br.com.dominio.model.Telefone;
-import br.com.dominio.model.Usuario;
-import br.com.dominio.util.ExceptionUtil;
-import br.com.dominio.util.JpaUtil;
-import java.util.ArrayList;
-import java.util.List;
-import javax.persistence.EntityManager;
-import org.junit.Assert;
-import org.junit.Test;
+//import br.com.dominio.dao.UsuarioDao;
+//import br.com.dominio.model.Telefone;
+//import br.com.dominio.model.Usuario;
+//import br.com.dominio.util.ExceptionUtil;
+//import br.com.dominio.util.JpaUtil;
+//import java.util.ArrayList;
+//import java.util.List;
+//import javax.persistence.EntityManager;
+//import org.junit.Assert;
+//import org.junit.Test;
+//
+//public class UsuarioDaoTest {
 
-
-public class UsuarioDaoTest {
-    
-    @Test
-    public void crearTest() throws ExceptionUtil {
-      EntityManager manager = JpaUtil.getEntityManager();
-      
-      Usuario usuario = new Usuario();
-      UsuarioDao usuarioDao = new UsuarioDao();
-      List<Telefone> telefones = new ArrayList<>();
-      
-      usuario.setNome("Juan Farias");
-      usuario.setEmail("juanfarias580@gmail.com");
-      usuario.setSenha("123");
-      
-      telefones.add(new Telefone(null, "81", "999966808", "Celular"));
-      usuario.setTelefones(telefones);
-      
-      usuarioDao.criar(usuario);
-      
-      Usuario usuarioCriado = manager.find(Usuario.class, usuario.getId());
-      
-      Assert.assertEquals(usuarioCriado.getTelefones(), usuario.getTelefones());
-    }
-    
-    @Test
-    public void atualizarTest() throws ExceptionUtil {
-      EntityManager manager = JpaUtil.getEntityManager();
-      
-      Usuario usuario = new Usuario();
-      UsuarioDao usuarioDao = new UsuarioDao();
-      List<Telefone> telefones = new ArrayList<>();
-      
-      usuario.setNome("Juan Farias");
-      usuario.setEmail("juanfarias580@gmail.com");
-      usuario.setSenha("123");
-      
-      telefones.add(new Telefone(null, "81", "999966808", "Celular"));
-      usuario.setTelefones(telefones);
-      
-      usuarioDao.criar(usuario);
-      Usuario usuarioCriado = manager.find(Usuario.class, usuario.getId());
-      String nomeCriado = usuarioCriado.getNome();
-      
-      usuarioCriado.setNome("Juan de Oliveira Farias");
-      usuarioCriado.setEmail("juanfarias580@gmail.com");
-      usuarioCriado.setSenha("123");
-      
-      telefones.add(new Telefone(null, "81", "999966808", "Celular"));
-      usuarioCriado.setTelefones(telefones);
-      
-      usuarioDao.atualizar(usuarioCriado);
-      Usuario usuarioAtualizado = manager.find(Usuario.class, usuario.getId());
-      String nomeAtualizado = usuarioAtualizado.getNome();
-      
-      Assert.assertNotEquals(nomeCriado, nomeAtualizado);
-    }
-}
+//    @Test
+//    public void crearTest() throws ExceptionUtil {
+//        EntityManager manager = JpaUtil.getEntityManager();
+//
+//        Usuario usuario = new Usuario();
+//        UsuarioDao usuarioDao = new UsuarioDao();
+//        List<Telefone> telefones = new ArrayList<>();
+//
+//        usuario.setNome("Juan Farias");
+//        usuario.setEmail("juanfarias580@gmail.com");
+//        usuario.setSenha("123");
+//
+//        telefones.add(new Telefone(null, "81", "999966808", "Celular"));
+//        usuario.setTelefones(telefones);
+//
+//        usuarioDao.criar(usuario);
+//
+//        Usuario usuarioCriado = manager.find(Usuario.class, usuario.getId());
+//
+//        Assert.assertEquals(telefones.toString(), 
+//                usuarioCriado.getTelefones().toString());
+//    }
+//
+//    @Test
+//    public void atualizarTest() throws ExceptionUtil {
+//        EntityManager manager = JpaUtil.getEntityManager();
+//
+//        Usuario usuario = new Usuario();
+//        UsuarioDao usuarioDao = new UsuarioDao();
+//        List<Telefone> telefones = new ArrayList<>();
+//
+//        usuario.setId(1);
+//        usuario.setNome("Juan de Oliveira Farias");
+//        usuario.setEmail("juanfarias580@gmail.com");
+//        usuario.setSenha("123");
+//
+//        telefones.add(new Telefone(1, "81", "999966808", "Celular"));
+//        telefones.add(new Telefone(2, "81", "999999747", "Celular"));
+//        usuario.setTelefones(telefones);
+//
+//        usuarioDao.atualizar(usuario);
+//
+//        Usuario usuarioAtualizado = manager.find(Usuario.class, usuario.getId());
+//
+//        Assert.assertEquals(telefones.toString(),
+//                usuarioAtualizado.getTelefones().toString());
+//    }
+//}
